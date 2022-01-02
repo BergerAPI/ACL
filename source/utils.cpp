f5be5b8ad8b8269d9124f7dd9a90de29a5f4aeee
peu77 <emilebert@icloud.com>
2022-01-02 19:14:25 +0000

    add copyright

--- source/utils.cpp
+++ source/utils.cpp
@@ -1,6 +1,18 @@
-//
-// Created by peu77 on 1/2/22.
-//
+/*
+ * Copyright (c) 2021/2022 BergerAPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "utils.h"
 
